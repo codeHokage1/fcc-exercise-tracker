04ae703d6a31c31f9431298f8829190b6f0c8645
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-11-16 09:25:40 +0100

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Exercise Tracker
 
-This is the boilerplate for the Exercise Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
+This is a project done in completion of my Backend & API development course on FreeCodeCamp. It allows for new users to be added and several tasks to be added/recorded for the corresponding user(s).
+
+Link to live API :link: : https://fcc-exercise-tracker-nine.vercel.app/
+
+```
+GET / 
+// displays the homepage
+
+
+GET /api/users
+// returns all users available in the database
+
+
+POST /api/users
+body: {
+  "username": "Farhan"
+}
+// creates a new user and returns a json that response that includes the _id
+
+
+POST /api/users/:_id/exercises
+body: {
+  "_id": "xxxxx12345",
+  "description": "Exercise 1 Done",
+  "duration": 10
+}
+// creates a new exercise for the user with corresponding _id and returns a json that response that includes the usernname and other response props
+
+
+GET /api/users/:_id/logs?[from][&to][&limit]
+// with only /logs; returns all exercises for the user with corresponding _id
+// returns all exercies for the user with corresponding _id while filtering the result according to the values of the optional from, to, and/or limit
+```
--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -38,52 +38,6 @@ exports.createExercise = async (req, res) => {
 }
 
 // get all exercises for a user
-// exports.getExercises = async (req, res) => {
-//     try {
-//         const _id = req.params._id;
-
-//         const limit = Number(req.query.limit) || 0;
-//         // const from = req.query.from || new Date(0);
-//         // const to = req.query.to || new Date(Date.now())
-
-//         const from = req.query.from ? new Date(req.query.from) : new Date(0);
-//         const to = req.query.to ? new Date(req.query.to) : new Date(Date.now());
-
-//         const foundUser = await UserModel.findOne({
-//             "_id": _id
-//         })
-
-//         if (!foundUser) return res.status(404).json({ "message": `User with id ${_id} not found` })
-//         const { username } = foundUser;
-//         const exercises = await ExerciseModel.find({
-//             "userId": _id,
-//             "date": { $gte: from, $lte: to }
-//         }).limit(limit);
-//         const count = exercises.length;
-//         const exercisesList = exercises.map(exercise => {
-//             return {
-//                 "description": exercise.description,
-//                 "duration": exercise.duration,
-//                 "date": exercise.date
-//             }
-//         })
-//         return res.json({
-//             "username": username,
-//             "count": count,
-//             "_id": _id,
-//             "log": exercisesList
-//         })
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).json({
-//             "message": "Server error"
-//         })
-//     }
-// }
-
-
-
-
 
 exports.getExercises = async (req, res) => {
     try {
